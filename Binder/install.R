b32dbfa4b475c52fadbbcee33ed33a69ec1c0f01
jen-reeve <31419037+jen-reeve@users.noreply.github.com>
2018-11-27 11:53:47 -0700

    Delete install.R

--- Binder/install.R
+++
@@ -1,7 +0,0 @@
-install.packages("tidyverse")
-# add any other packages needed to run the repo here
-install.packages("openxlsx") # reading and writing excel file
-install.packages("knitr") # generating reports
-install.packages("plotly") # interactive plots
-install.packages("CHNOSZ") # stability diagrams
-install.packages("latex2exp") # ability to write more complex things on figures
